88eb7ea05debf859b8978edb5e8919fdb8d79b9c
Isen <2731871024@qq.com>
2021-02-01 20:03:28 +0800

    切换问题修复

--- jpress/jpress-template/src/main/webapp/templates/DisourceDatabase/static/js/content.js
+++ jpress/jpress-template/src/main/webapp/templates/DisourceDatabase/static/js/content.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    $(".nav li").click(function(){
+    $(".nav li").click(function () {
         $(".nav li").removeClass("active");
         $(this).addClass("active");
-        if($(this).find("a").html()=="分类"){
-            $(".info-inner").eq(0).css("display","");
-            $(".info-inner").eq(1).css("display","none");
-        }else if ($(this).find("a").html()=="来源"){
-            $(".info-inner").eq(0).css("display","none");
-            $(".info-inner").eq(1).css("display","");
+        if ($(this).find("a").html() == "categorization") {
+            $(".info-inner").eq(0).css("display", "");
+            $(".info-inner").eq(1).css("display", "none");
+        } else if ($(this).find("a").html() == "source") {
+            $(".info-inner").eq(0).css("display", "none");
+            $(".info-inner").eq(1).css("display", "");
         }
     });
 });
